9c4360c7b3b413cf8e7c6448ec0bb93c76f87d06
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-04-29 07:15:06 +0200

    start bulid configration circleCi
    start bulid configration circleCi

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// ENV variables 
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+// Are Also needed
+
+export const config = {
+  PORT: process.env.PORT,
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  DBport: Number(process.env.DBPORT),
+  host: process.env.POSTGRES_HOST,
+  dialect: "postgres",
+  awsRegion: process.env.AWS_REGION,
+  awsProfile: process.env.AWS_PROFILE,
+  awsMediaBucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
+
