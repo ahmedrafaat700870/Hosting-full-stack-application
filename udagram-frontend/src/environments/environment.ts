c6832f895044bd86ff57d6d2bde16b59cec9a858
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-05-02 05:12:46 +0200

    update environments front-end

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'udagram-api-dev.eba-hsfea22b.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
