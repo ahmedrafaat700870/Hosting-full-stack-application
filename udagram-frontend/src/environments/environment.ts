ffd1dcefd3419f3d88f7e5bca92a552615370775
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-05-02 05:17:40 +0200

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
   apiHost: 'udagram-api-dev.eba-hsfea22b.us-east-1.elasticbeanstalk.com/api/v0'
 };
