99a7728a3b6d1c4714d3f4b01f1e50e1a3bfaa2d
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-05-03 10:09:59 +0200

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,9 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "udagram-api-dev.eba-hsfea22b.us-east-1.elasticbeanstalk.com/api/v0/",
+  apiHost: "http://udagram-api-dev.eba-hsfea22b.us-east-1.elasticbeanstalk.com//api/v0",
 };
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: ''
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
